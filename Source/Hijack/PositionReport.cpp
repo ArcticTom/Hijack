b5ec50be660896a17893b172131f043255216a54
Tommi Kujanen <tommi.kujanen@gmail.com>
2018-05-01 20:57:06 +0300

    Hijack - Section 1

---
+++ Source/Hijack/PositionReport.cpp
@@ -0,0 +1,36 @@
+// Copyright Tommi Kujanen 2018.
+
+#include "PositionReport.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UPositionReport::UPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Object reporting in: %s"), *ObjectName);
+	
+}
+
+
+// Called every frame
+void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
