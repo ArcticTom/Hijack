f950898ea767e715c2a273337aeff7a0fa98ca9b
Tommi Kujanen <tommi.kujanen@gmail.com>
2018-05-05 22:48:59 +0300

    Hijack - Section 2

---
+++ Source/Hijack/OpenDoor.cpp
@@ -0,0 +1,64 @@
+// Copyright Tommi Kujanen 2018.
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Owner = GetOwner();
+
+	PressureTriggerer = GetWorld()->GetFirstPlayerController()->GetPawn();
+	
+}
+
+void UOpenDoor::OpenDoor() {
+
+	//calculate new rotation
+	FRotator NewRotation = FRotator(0.0f, (Owner->GetActorRotation().Yaw + OpenAngle), 0.0f);
+
+	Owner->SetActorRotation(NewRotation);
+
+	IsOpen = true;
+}
+
+void UOpenDoor::CloseDoor() {
+
+	//calculate new rotation
+	FRotator NewRotation = FRotator(0.0f, (Owner->GetActorRotation().Yaw + -OpenAngle), 0.0f);
+
+	Owner->SetActorRotation(NewRotation);
+
+	IsOpen = false;
+}
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (PressurePlate->IsOverlappingActor(PressureTriggerer) && !IsOpen) {
+		OpenDoor();
+		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+	}
+	
+	if ((GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay) && IsOpen) {
+		CloseDoor();
+	}
+	
+}
+
--- Source/Hijack/PositionReport.cpp
+++ Source/Hijack/PositionReport.cpp
@@ -21,7 +21,8 @@ void UPositionReport::BeginPlay()
 	Super::BeginPlay();
 
 	FString ObjectName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("Object reporting in: %s"), *ObjectName);
+	FString ObjectPosition = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPosition);
 	
 }
 
